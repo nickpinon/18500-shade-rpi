83a4bb0aa1946462fb2626f0e15a39d7659e1739
nickpinon <npinon@andrew.cmu.edu>
2026-03-30 00:32:57 -0400

    new branch with lsm9ds1 sensor code

---
+++ sensor/lsm9ds1.py
@@ -0,0 +1,7 @@
+'''
+Inspired by LSM9DS1_Basic_I2C.ino from Adafruit's LSM9DS1 library:
+https://github.com/sparkfun/SparkFun_LSM9DS1_Arduino_Library/blob/master/examples/LSM9DS1_Basic_I2C/LSM9DS1_Basic_I2C.ino
+
+Referenced this repo for Mahony AHRS implementation:
+https://github.com/jremington/LSM9DS1-AHRS/blob/main/LSM9DS1_cal_data/LSM9DS1_cal_data.ino 
+'''

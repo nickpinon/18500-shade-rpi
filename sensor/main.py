fe94ff6d2a2b9c0d20de8e5c4d103f87c4a0bd35
nickpinon <npinon@andrew.cmu.edu>
2026-03-30 02:40:30 -0400

    attempted mahony

---
+++ sensor/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from lsm9ds1 import LSM9DS1
+from mahony_fusion import MahonyFilter
+import time
+
+def main():
+    imu = LSM9DS1()
+    imu.calibrate()
+    imu.calibrate_mag()
+    fusion = MahonyFilter(kp=0.5, ki=0.0)
+
+    print("Attempting AHRS")
+    print("Coordinates: Roll(X), Pitch(Y), Yaw(Z) in degrees")
+
+
+
+    while True:
+        accel = imu.read_accel()
+        gyro = imu.read_gyro()
+        mag = imu.read_mag()
+        fusion.update(gyro, accel, mag)
+        roll, pitch, yaw = fusion.update(gyro, accel, mag)
+        output = f"Roll: {roll:>7.2f}° | Pitch: {pitch:>7.2f}° | Yaw: {yaw:>7.2f}°"
+        print(output, end="\r", flush=True)
+        time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()

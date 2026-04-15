f27a695de791e8a9db8d7a5aba93b25525f30315
nickpinon <npinon@andrew.cmu.edu>
2026-04-15 06:43:08 -0400

    print debugging

--- sensor/lsm6dsox/main.py
+++ sensor/lsm6dsox/main.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import time
+import math
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from lsm6dsox import LSM6DSOX
@@ -23,7 +24,13 @@ def main():
     while True:
         accel = imu.read_accel()
         gyro = imu.read_gyro()
+        # Inside your while loop in lsm6dsox/main.py
         mag = mag_sensor.read_mag()
+        # Calculate a simple 2D heading from the mag alone (in degrees)
+        mag_heading = math.degrees(math.atan2(mag[1], mag[0]))
+        mag_magnitude = math.sqrt(mag[0]**2 + mag[1]**2 + mag[2]**2)
+
+        print(f"Mag Heading: {mag_heading:>7.2f} | Strength: {mag_magnitude:>7.2f}", end="\r")
 
         # Fusion now has real data for all 9 axes
         roll, pitch, yaw = fusion.update(gyro, accel, mag)

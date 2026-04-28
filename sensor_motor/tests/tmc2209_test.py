0968365303a76e44bacc9ba85b906bc8a1ea57c5
nickpinon <npinon@andrew.cmu.edu>
2026-04-28 03:09:00 -0400

    faster speed

--- sensor_motor/tests/tmc2209_test.py
+++ sensor_motor/tests/tmc2209_test.py
@@ -1,48 +1,34 @@
 import time
 import lgpio
 
-# Original Horizontal Motor Pins
 STEP_PIN = 17
 DIR_PIN = 27
 EN_PIN = 22
 
-# 100Hz Frequency (10ms period)
-STEP_DELAY = 0.005 
+# Faster speed: 1000Hz (1ms period)
+# At 1000Hz, the output shaft will move at ~0.1 RPM (still slow, but visible!)
+STEP_DELAY = 0.0005 
 
 def run_test():
-    print("--- Reverting to Original Continuous Test ---")
-    
-    try:
-        chip = lgpio.gpiochip_open(0)
-        lgpio.gpio_claim_output(chip, STEP_PIN, 0)
-        lgpio.gpio_claim_output(chip, DIR_PIN, 0)
-        lgpio.gpio_claim_output(chip, EN_PIN, 1) # Start High/Disabled
-        print("GPIO initialized. Probing STEP (17) and EN (22)...")
-    except Exception as e:
-        print(f"GPIO Error: {e}")
-        return
+    chip = lgpio.gpiochip_open(0)
+    lgpio.gpio_claim_output(chip, STEP_PIN, 0)
+    lgpio.gpio_claim_output(chip, DIR_PIN, 0)
+    lgpio.gpio_claim_output(chip, EN_PIN, 1)
 
     try:
-        # Enable the driver
-        print("Setting Enable LOW (0V)...")
-        lgpio.gpio_write(chip, EN_PIN, 0)
+        lgpio.gpio_write(chip, EN_PIN, 0) # Enable
         time.sleep(0.5)
-
-        print("Starting 100Hz pulses. Press Ctrl+C to stop.")
-        lgpio.gpio_write(chip, DIR_PIN, 1)
+        print("Moving... Watch the output shaft VERY closely (put a piece of tape on it!)")
         
         while True:
             lgpio.gpio_write(chip, STEP_PIN, 1)
             time.sleep(STEP_DELAY)
             lgpio.gpio_write(chip, STEP_PIN, 0)
             time.sleep(STEP_DELAY)
-
     except KeyboardInterrupt:
-        print("\nStopping...")
+        pass
     finally:
-        lgpio.gpio_write(chip, EN_PIN, 1) # Disable motor
+        lgpio.gpio_write(chip, EN_PIN, 1)
         lgpio.gpiochip_close(chip)
-        print("Cleanup done.")
 
-if __name__ == "__main__":
-    run_test()
+run_test()
